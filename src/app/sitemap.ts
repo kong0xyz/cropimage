3092a7de04f9a60e6e8f5f232062ac25d048f495
kong <kong0xyz@gmail.com>
2025-04-28 21:58:24 +0800

    feat: sitemap, remove next-sitemap, use next.js sitemap function

---
+++ src/app/sitemap.ts
@@ -0,0 +1,47 @@
+import { i18n } from "@/config/i18n.config";
+import { source } from "@/lib/source";
+import type { MetadataRoute } from "next";
+
+const locales = i18n.locales;
+const defaultLocale = i18n.defaultLocale;
+const staticPaths = ["", "/about", "/privacy", "/terms", "/pricing"];
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const urls: MetadataRoute.Sitemap = [];
+
+  // static
+  for (const path of staticPaths) {
+    // 创建一个符合MetadataRoute.Sitemap中alternates.languages类型的对象
+    const languages: Record<string, string> = {};
+
+    locales
+      .filter((locale) => locale !== defaultLocale)
+      .forEach((locale) => {
+        languages[locale] = `${process.env.SITE_URL}/${locale}${path}`;
+      });
+
+    urls.push({
+      url: `${process.env.SITE_URL}${path}`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+      alternates: {
+        languages,
+      },
+    });
+  }
+
+  // fumadocs
+  const entries = source.getLanguages();
+  const docsurls = entries.flatMap(({ language, pages }) =>
+    pages.map((page) => ({
+      url: `${process.env.SITE_URL}${page.url}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly" as const,
+      priority: 1,
+    }))
+  );
+  urls.push(...docsurls);
+
+  return urls;
+}
