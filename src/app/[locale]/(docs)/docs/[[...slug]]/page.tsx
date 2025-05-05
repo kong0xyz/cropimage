2214ae9e34d677dacd5b60b7f24d4c8898cd9b30
kong <kong0xyz@gmail.com>
2025-05-05 21:09:13 +0800

    fix: typograph dark

--- src/app/[locale]/(docs)/docs/[[...slug]]/page.tsx
+++ src/app/[locale]/(docs)/docs/[[...slug]]/page.tsx
@@ -23,7 +23,7 @@ export default async function Page(props: {
         }} toc={page.data.toc} full={page.data.full}>
             <DocsTitle>{page.data.title}</DocsTitle>
             <DocsDescription>{page.data.description}</DocsDescription>
-            <DocsBody>
+            <DocsBody className="dark:prose-invert">
                 <MDX components={getMDXComponents()} />
             </DocsBody>
         </DocsPage>
