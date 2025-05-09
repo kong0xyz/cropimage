d9c3af439114e61c956c6d0aaa0cb3ea2f1c5245
kong <kong0xyz@gmail.com>
2025-05-09 20:25:51 +0800

    fix

--- src/components/landing/basic-hero.tsx
+++ src/components/landing/basic-hero.tsx
@@ -10,7 +10,7 @@ export default function BasicHero() {
           preset="fade-in-blur"
           speedSegment={0.3}
           as="h1"
-          className="mt-8 text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem]">
+          className="mt-8 text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem] text-primary">
           {hero.title}
         </TextEffect>
       )}
