4fdd7d10eeef79cdc98f13c2e89340d94932296d
kong <kong0xyz@gmail.com>
2025-07-05 00:48:04 +0800

    fix: .cursor/rules

---
+++ src/app/[locale]/(main)/_feature/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+// import { useTranslations } from "next-intl";
+
+export default function FeaturePage() {
+  // const t = useTranslations("feature"); // useTranslations is not working in client component
+
+  return (
+    <section>
+      <p>Feature Main Content Page</p>
+    </section>
+  );
+} 
