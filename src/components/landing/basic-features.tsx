b97e1fd0a9d7b025c3646b9f65fa87b84c439e08
kong <kong0xyz@gmail.com>
2025-12-06 00:51:45 +0800

    fix

--- src/components/landing/basic-features.tsx
+++ src/components/landing/basic-features.tsx
@@ -9,7 +9,7 @@ import { FeaturesSectionWithHoverEffects } from "../common/feature-section-with-
 export default function BasicFeatures() {
   return (
     <section>
-      <div className="flex flex-col items-center gap-4 text-center container mx-auto">
+      <div className="flex flex-col items-center gap-4 text-center container mx-auto px-4 lg:px-6">
         <div className="flex flex-col items-center gap-4 px-4 pb-6">
           <h2 className="max-w-[720px] text-3xl font-semibold leading-tight sm:text-5xl sm:leading-tight">
             {featureTitle}
